fd149c7d7cbbead3a38803565f7732645995b6e5
Grensel <sergeiodinochenko@mail.ru>
2024-08-10 13:46:10 +0300

    feat: completed an algorithmic task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const positiveNumber = Math.abs(n);
+  const arrayOfNumber = Array.from(String(positiveNumber));
+  const arrayRevers = arrayOfNumber.reverse();
+  const arraySum = +arrayRevers.join('');
+  return arraySum;
 }
